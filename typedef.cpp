7ac365df805e61053587af6d1fe797f208c8aa77
xpcosmos <mikeias.d.s.o@gmail.com>
2025-03-29 10:56:20 -0300

    new: Definindo tipos personalizados 🧩

---
+++ typedef.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+typedef std::string text_t;
+typedef int number_t;
+
+int main() {
+    using std::cout;
+    using std::endl;
+
+    text_t text;
+    number_t age;
+
+    text = "Mikeias";
+    age = 23;
+
+    cout << text << endl;
+    cout << age << endl;
+
+    return 0;
+}
