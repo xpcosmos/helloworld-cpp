40dcb78b2e6f04c750dc7692118259c4c2c95f87
xpcosmos <mikeias.d.s.o@gmail.com>
2025-03-28 21:35:32 -0300

    feature: Nosso codigo agora faz soma ➕

--- main.cpp
+++ main.cpp
@@ -4,9 +4,15 @@ int main(){
     
     // Declaracao
     int x;
+
     x = 4;
-    
-    std::cout << x;
+    int y = 6;
+
+    int sum = x + y;
+
+    std::cout << x << '\n';
+    std::cout << y << '\n';
+    std::cout << sum;
 
     return 0;
 }
